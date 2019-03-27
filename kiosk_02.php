83f179d9e6c19be51e3d3be5cfd2c37e374409d0
Bentley <bentley.hudson@henryschein.com>
2019-03-27 09:22:59 -0500

    button alignment

--- kiosk_02.php
+++ kiosk_02.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Break/Lunch Kiosk</title>
+        <?php include_once '../printvis/headerincludes.php'; ?>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+    </head>
+
 <?php
 include_once '../connections/conn_printvis.php';
 
