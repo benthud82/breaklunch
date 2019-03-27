8d776395a13b2844f417d132cb7cd135e996d50a
Bentley Hudson <bentley.hudson@henryschein.com>
2019-03-27 10:28:38 -0400

    test

--- kiosk_02.php
+++ kiosk_02.php
@@ -2,8 +2,7 @@
 <html>
 
     <head>
-        <title>Break/Lunch Kiosk</title>
-        <?php include_once '../printvis/headerincludes.php'; ?>
+
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 
