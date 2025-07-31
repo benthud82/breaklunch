9452c74fb35ef2c03430def86e43af8487e864d6
Your Name <benthud82@gmail.com>
2025-07-31 14:30:56 -0500

    Update FTP upload script to improve error handling and streamline data processing. Enhanced SQL queries for better performance and removed deprecated functions.

---
+++ kiosk_10.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    </head>
+<?php
+include_once '../connections/conn_printvis.php';
+
+$_SESSION['whse'] = 10;
+$whse = 10;
+include 'kiosk_signin_NOTL.php';
